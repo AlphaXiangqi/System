81917f9411ff0608f0f49471e55324735b7a6820
Yanqing Wu <y433wu@edu.uwaterloo.ca>
2018-04-17 23:29:08 -0400

    update UI

--- src/web/client.py
+++ src/web/client.py
@@ -7,5 +7,5 @@ def home():
     return render_template('main.html')
  
 if __name__ == '__main__':
-    app.run()
-
+    app.config['TEMPLATES_AUTO_RELOAD'] = True
+    app.run(debug=True,host='0.0.0.0', port=4000)
